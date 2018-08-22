085befa1c56a8a20a58188bee1afdfc0ea79816f
Jörgen Nihlgård <j.nihlgard@outlook.com>
2018-08-22 09:32:01 +0200

    Finished Module 2

---
+++ Orchard.Web/Modules/Pluralsight.Movies/Migrations.cs
@@ -0,0 +1,45 @@
+﻿using Orchard.ContentManagement.MetaData;
+using Orchard.Core.Contents.Extensions;
+using Orchard.Data.Migration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Pluralsight.Movies
+{
+    public class Migrations : DataMigrationImpl
+    {
+        public int Create()
+        {
+            ContentDefinitionManager.AlterTypeDefinition("Movie", builder=>
+            builder.WithPart("CommonPart")
+            .WithPart("TitlePart")
+            .WithPart("AutoRoutePart"));
+            return 1;
+        }
+
+        public int UpdateFrom1()
+        {
+            ContentDefinitionManager.AlterTypeDefinition("Movie", builder =>
+            builder.WithPart("BodyPart")
+            .Creatable()
+            .Draftable());
+            return 2;
+        }
+        public int UpdateFrom2()
+        {
+            ContentDefinitionManager.AlterTypeDefinition("Movie", builder =>
+                builder.WithPart("BodyPart", partBuilder =>
+                partBuilder.WithSetting("BodyTypePartSettings.Flavor", "text"))
+            .WithPart("AutoroutePart",partBuilder=>
+            partBuilder
+            .WithSetting("AutorouteSettings.AllowCustomPattern", "true")
+                        .WithSetting("AutorouteSettings.AutomaticAdjustmentOnEdit", "false")
+                        .WithSetting("AutorouteSettings.PatternDefinitions", "[{Name:'Movie Title', Pattern: 'movies/{Content.Slug}', Description: 'movies/movie-title'}, {Name:'Film Title', Pattern: 'films/{Content.Slug}', Description: 'films/film-title'}]")
+                        .WithSetting("AutorouteSettings.DefaultPatternIndex", "0")));
+            return 3;
+        }
+    }
+   
+}
